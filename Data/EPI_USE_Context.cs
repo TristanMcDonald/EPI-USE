241180118114eda5793492fbc6e25dbc4901c130
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-10-18 20:06:02 +0200

    Enabled Entity Framework Migrations and created the initial database

---
+++ Data/EPI_USE_Context.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace EPI_USE.Data
+{
+    public class EPI_USE_Context : DbContext
+    {
+        public EPI_USE_Context()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace EPI_USE.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EPI_USE.Data.EPI_USE_Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EPI_USE.Data.EPI_USE_Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
