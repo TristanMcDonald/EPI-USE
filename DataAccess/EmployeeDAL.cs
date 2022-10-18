499c6ec0fd92d05c7ca806112d7584d9608f858a
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-10-18 20:57:50 +0200

    Added an Employee data access layer that contains methods to create, read, update, and delete employees

---
+++ DataAccess/EmployeeDAL.cs
@@ -0,0 +1,69 @@
+﻿using EPI_USE.Data;
+using EPI_USE.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EPI_USE.DataAccess
+{
+    public class EmployeeDAL
+    {
+        private EPI_USE_Context epi_use_context;
+        public EmployeeDAL()
+        {
+            epi_use_context = new EPI_USE_Context();
+        }
+
+        //Method to Create a new employee by adding their information entered to the database (context).
+        public void CreateEmployee(Employee employee)
+        {
+            //Adding the new Employee to the database employees.
+            epi_use_context.Employees.Add(employee);
+            //Saving the changes made to the database context.
+            epi_use_context.SaveChanges();
+        }
+
+        //Method to fetch all Employees.
+        public IEnumerable<Employee> GetAllEmployees(string employeeNumber)
+        {
+            //initializing the Employees list.
+            List<Employee> allEmployees = new List<Employee>();
+
+            foreach (var employee in epi_use_context.Employees)
+            {
+                allEmployees = new List<Employee>
+                    (from emp in epi_use_context.Employees
+                     select emp
+                    );
+            }
+            return allEmployees;
+        }
+
+        //Method to get a specific employee
+        public Employee GetEmployee(string employeeNumber)
+        {
+            Employee chosenEmployee = new Employee();
+
+            chosenEmployee = (Employee)(from emp in epi_use_context.Employees
+                             where emp.EmployeeNumber.Equals(employeeNumber)
+                             select emp);
+
+            return chosenEmployee;
+        }
+
+        //Method to update an employees information
+        public void UpdateEmployee()
+        {
+
+        }
+
+        //Method to remove an employee from the database
+        public void DeleteEmployee(Employee employee)
+        {
+            //Removing the specified Employee from the database employees.
+            epi_use_context.Employees.Remove(employee);
+            //Saving the changes made to the database context.
+            epi_use_context.SaveChanges();
+        }
+
+    }
+}
