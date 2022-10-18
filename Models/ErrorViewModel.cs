fd1802ef9e5001e309bd5e57be225b03583ed3e4
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-10-18 19:35:15 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EPI_USE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
